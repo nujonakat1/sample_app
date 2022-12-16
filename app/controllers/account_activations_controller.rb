081ffeee340ad27e8a6a73b4db54e5fa43611d1b
nujonakat1 <takanojun@icloud.com>
2022-12-16 05:27:55 +0000

    Add account activationの修正

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,14 +1,16 @@
 class AccountActivationsController < ApplicationController
-
+  # paramsハッシュで渡されたメールアドレスに対応するユーザーを認証する
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      # 有効だった場合、ユーザーを認証してから activated_at タイムスタンプを更新
       user.update_attribute(:activated,    true)
       user.update_attribute(:activated_at, Time.zone.now)
       log_in user
       flash[:success] = "Account activated!"
       redirect_to user
     else
+      # 有効化トークンが無効だった場合の処理
       flash[:danger] = "Invalid activation link"
       redirect_to root_url
     end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,39 +2,24 @@ class SessionsController < ApplicationController
   def new
   end
 
-  # def create
-  #   @user = User.find_by(email: params[:session][:email].downcase)
-  #   # if user && user.authenticate(params[:session][:password])
-  #   if @user&.authenticate(params[:session][:password])    #ぼっち演算子で記述
-  #     if user.activated?
-  #       forwarding_url = session[:forwarding_url]   # 最初にセッションを取得
-  #       # ユーザーログイン後にユーザー情報のページにリダイレクトする
-  #       reset_session      # ログインの直前に必ずこれを書くこと
-  #       # remember user      # ログインしてユーザーを保持する
-  #       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-  #       log_in @user
-  #       # redirect_to @user
-  #       redirect_to forwarding_url || @user     # 転送先かプロフへリダイレクト
-  #     else
-  #       message  = "Account not activated. "
-  #       message += "Check your email for the activation link."
-  #       flash[:warning] = message
-  #       redirect_to root_url
-  #     end
-  #   else
-  #     # エラーメッセージを作成する
-  #     flash.now[:danger] = 'Invalid email/password combination'
-  #     render 'new', status: :unprocessable_entity
-  #   end
-  # end
+  # 有効でないユーザーがログインすることのないようにする
   def create
+    # @user = User.find_by(email: params[:session][:email].downcase)
     user = User.find_by(email: params[:session][:email].downcase)
+    # if user && user.authenticate(params[:session][:password])
+    # if @user&.authenticate(params[:session][:password])    #ぼっち演算子で記述
     if user && user.authenticate(params[:session][:password])
-      if user.activated?
-        forwarding_url = session[:forwarding_url]
-        reset_session
+      if user.activated?    # true の場合にのみログインを許可
+        forwarding_url = session[:forwarding_url]   # 最初にセッションを取得
+        # ユーザーログイン後にユーザー情報のページにリダイレクトする
+        reset_session      # ログインの直前に必ずこれを書くこと
+        # remember user      # ログインしてユーザーを保持する
+        # params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        # log_in @user
         log_in user
+        # redirect_to @user
+        # redirect_to forwarding_url || @user     # 転送先かプロフへリダイレクト
         redirect_to forwarding_url || user
       else
         message  = "Account not activated. "
@@ -43,6 +28,7 @@ class SessionsController < ApplicationController
         redirect_to root_url
       end
     else
+      # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
     end
