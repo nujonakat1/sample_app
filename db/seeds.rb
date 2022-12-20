9d3fa825223f37853f3e1bbaa1165e511263c306
nujonakat1 <takanojun@icloud.com>
2022-12-20 14:53:10 +0000

    Add user following(修正)

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ User.create!(name:  "Example User",
 end
 
 # ユーザーの一部を対象にマイクロポストを生成する
-users = User.order(:created_at).take(3)
+users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
