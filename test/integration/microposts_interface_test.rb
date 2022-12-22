718cfdf00554374c179b2de9ef57d9593869e7fe
nujonakat1 <takanojun@icloud.com>
2022-12-22 15:09:15 +0000

    solve the exercises2

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -70,3 +70,21 @@ class MicropostSidebarTest < MicropostsInterface
     assert_match "1 micropost", response.body
   end
 end
+
+# 画像アップローダーをテスト
+# Home にあるファイルアップロードと投稿に成功した時に画像が表示されているか
+class ImageUploadTest < MicropostsInterface
+
+  test "should have a file input field for images" do
+    get root_path
+    assert_select 'input[type="file"]'
+  end
+
+  test "should be able to attach an image" do
+    cont = "This micropost really ties the room together."
+    # fixtureで定義されたファイルをアップロードする特別なメソッド
+    img  = fixture_file_upload('kitten.jpg', 'image/jpeg')
+    post microposts_path, params: { micropost: { content: cont, image: img } }
+    assert assigns(:micropost).image.attached?
+  end
+end
